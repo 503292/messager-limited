f1f28b4e74442473decb6830c7401a6efe10c409
kalysh_maxym <503292@ukr.net>
2020-07-24 12:35:53 +0300

    ADD _ users.js temlate to view info from users/ ADD _ all images

---
+++ src/users.js
@@ -0,0 +1,92 @@
+import user1 from "./assets/img/users/user1.png";
+import user2 from "./assets/img/users/user2.png";
+import user3 from "./assets/img/users/user3.png";
+import user4 from "./assets/img/users/user4.png";
+import user5 from "./assets/img/users/user5.png";
+import user6 from "./assets/img/users/user6.png";
+import user7 from "./assets/img/users/user7.png";
+
+export default {
+  users: [
+    {
+      imgUrl: user1,
+      userName: "Sarah Freeman",
+      infoUser: "Excepteur sint occae...",
+      dateTime: "9:32",
+      online: true,
+
+      messages: [
+        {
+          message: "To take a trivial example? which of us ever undertakes)",
+          imgUserMessage: "",
+          time: "01:38",
+        },
+        {
+          message:
+            "But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures ",
+          imgUserMessage: user4,
+          time: "01:14",
+        },
+        {
+          message: "Which is the same as saying?",
+          imgUserMessage: "",
+          time: "01:38",
+        },
+        {
+          message:
+            "On the other hand, we denounce with righteous indignation! ",
+          imgUserMessage: user4,
+          time: "01:14",
+        },
+      ],
+    },
+    {
+      imgUrl: user2,
+      userName: "Miranda Hops",
+      infoUser: "Excepteur sint occae...",
+      dateTime: "9:32",
+      online: true,
+      messages: [],
+    },
+    {
+      imgUrl: user3,
+      userName: "Barry Callebaut",
+      infoUser: "Nemo enim ipsam volupta…",
+      dateTime: "22:01",
+      online: false,
+      messages: [],
+    },
+    {
+      imgUrl: user4,
+      userName: "Stacey Dosh",
+      infoUser: "Ut enim ad minima ve",
+      dateTime: "9:15",
+      online: true,
+      messages: [],
+    },
+    {
+      imgUrl: user5,
+      userName: "Michael Kors",
+      infoUser: "To take a trivial example…",
+      dateTime: "10:25",
+      online: false,
+      messages: [],
+    },
+    {
+      imgUrl: user6,
+      userName: "Johny Cash",
+      infoUser: "Great explorer of the truth…",
+      dateTime: "10:25",
+      online: true,
+      messages: [],
+    },
+    {
+      imgUrl: user7,
+      userName: "Stacey Dosh",
+      infoUser: "Ut enim ad minima veniam…",
+      dateTime: "08:53",
+      online: false,
+      messages: [],
+    },
+  ],
+};
