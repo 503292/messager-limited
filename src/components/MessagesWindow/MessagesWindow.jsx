1a0eb34f5076030c50cae3ec402f574093502e0b
kalysh_maxym <503292@ukr.net>
2020-07-24 17:36:27 +0300

    VIEW _ Users in MessagesWindow

--- src/components/MessagesWindow/MessagesWindow.jsx
+++ src/components/MessagesWindow/MessagesWindow.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import Users from "./Users/Users";
 
 import css from "./MessagesWindow.module.scss";
 
-const MessagesWindow = () => {
+const MessagesWindow = ({ users }) => {
   return (
     <div className={css.wrapMessagesWindow}>
-      <Users />
+      <Users users={users} />
       <div className={css.wrapMessages}>wrapMessages</div>
     </div>
   );
+};
+
+MessagesWindow.propTypes = {
+  users: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
 };
 
 export default MessagesWindow;
--- src/components/MessagesWindow/Users/Users.jsx
+++ src/components/MessagesWindow/Users/Users.jsx
@@ -1,34 +1,53 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import { ListGroup, Figure } from "react-bootstrap";
+import { ReactComponent as Circle } from "../../../assets/img/circle.svg";
+import { ReactComponent as Voice } from "../../../assets/img/request.svg";
 
 import css from "./Users.module.scss";
 
-const Users = () => {
+const Users = ({ users }) => {
   return (
     <ListGroup className={css.usersUl}>
-      <ListGroup.Item className={css.usersLi}>
-        <Figure>
-          <Figure.Image
-            width={50}
-            height={50}
-            alt="50*50"
-            src="holder.js/171x180"
-          />
-          <Figure.Caption>
-            <div>
-              <p>Name</p>
-              <p>.</p>
-              <p>voice</p>
-            </div>
-            <p>info</p>
-          </Figure.Caption>
+      {users.map((u) => {
+        if (u.id === "1") {
+          return "";
+        } else {
+          return (
+            <ListGroup.Item className={css.usersLi}>
+              <Figure className={css.oneUser}>
+                <Figure.Image
+                  width={50}
+                  height={50}
+                  alt="50*50"
+                  src={u.imgUrl}
+                />
+                <Figure.Caption className={css.wrapInfo}>
+                  <div className={css.wrapDescription}>
+                    <p className={css.userName}>{u.userName}</p>
+                    {u.online && <Circle />}
+                    {!u.voice && (
+                      <div>
+                        <Voice />
+                      </div>
+                    )}
+                  </div>
+                  <p className={css.userInfo}>{u.userInfo}</p>
+                </Figure.Caption>
 
-          <p>time</p>
-        </Figure>
-      </ListGroup.Item>
+                <p className={css.dateTime}>{u.dateTime}</p>
+              </Figure>
+            </ListGroup.Item>
+          );
+        }
+      })}
     </ListGroup>
   );
+};
+
+Users.propTypes = {
+  users: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
 };
 
 export default Users;
