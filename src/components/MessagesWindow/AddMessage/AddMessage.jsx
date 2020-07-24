20541b05e7bdb26957263f2f2ca0e5756ca9d2c9
kalysh_maxym <503292@ukr.net>
2020-07-24 18:51:51 +0300

    ADD _ AddMessage

--- src/components/MessagesWindow/AddMessage/AddMessage.jsx
+++ src/components/MessagesWindow/AddMessage/AddMessage.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 
+import { ReactComponent as Smile } from "../../../assets/img/smiling-emotion-square-face.svg";
+import { ReactComponent as Clip } from "../../../assets/img/clip.svg";
+import { ReactComponent as Send } from "../../../assets/img/send-message-button.svg";
+
 import css from "./AddMessage.module.scss";
 
 const AddMessage = () => {
-  return <div className={css.wrapAddMessage}>AddMessage</div>;
+  return (
+    <div className={css.wrapAddMessage}>
+      <input className={css.input} type="text" placeholder="Write a message…" />
+      <div className={css.wrapButton}>
+        <button>
+          <Smile />
+        </button>
+        <button>
+          <Clip />
+        </button>
+        <button>
+          <Send />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default AddMessage;
