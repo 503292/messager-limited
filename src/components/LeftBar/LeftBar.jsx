3c914c62f1d48f43383fac9e6bfb6903e1469751
kalysh_maxym <503292@ukr.net>
2020-07-24 12:42:28 +0300

    ADD _ big nodes in project -> do global structure components

---
+++ src/components/LeftBar/LeftBar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import css from "./LeftBar.module.scss";
+
+const LeftBar = () => {
+  return <div>LeftBar</div>;
+};
+
+export default LeftBar;
---
+++ src/components/MessagesWindow/MessagesWindow.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import css from "MessagesWindow.module.scss";
+
+const MessagesWindow = () => {
+  return <div>MessagesWindow</div>;
+};
+
+export default MessagesWindow;
