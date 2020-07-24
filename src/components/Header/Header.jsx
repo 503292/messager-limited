d01d15d38cba6fa77d826e7465c3962eb21c9308
kalysh_maxym <503292@ukr.net>
2020-07-24 13:51:34 +0300

    ADD _ breadCrumbs to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 
+import { Breadcrumb } from "react-bootstrap";
+
 import css from "./Header.module.scss";
 
 const Header = () => {
-  return <div className={css.wrapHeader}>Header</div>;
+  return (
+    <div className={css.wrapHeader}>
+      {/* <div className={css.breadCrumbs}> */}
+      <Breadcrumb className={css.breadCrumbs}>
+        <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+        <Breadcrumb.Item href="#">Network</Breadcrumb.Item>
+      </Breadcrumb>
+      {/* <p>Home</p> */}
+      {/* <img alt="arrow-right" />
+        <p>Network</p> */}
+      {/* </div> */}
+      <div className={css.userInfo}>
+        <img alt="foto user" />
+        <p>Sarah Freman</p>
+        <img alt="arrow bottom" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
